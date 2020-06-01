215fd3c5194da222f4db2d0e92d2cb3c01765b19
jjongrrr@naver.com <jjongrrr@naver.com>
2020-06-01 17:48:14 +0900

    'first'

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,8 +3,22 @@ import React, { Component } from 'react'
 class Item extends Component {
 
     state = {
-        updateBox: !false,
+        updateBox: false,
+        name: '',
+        password: '',
     }
+
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
 
     handleRemove = () => {
         const { info, onRemove } = this.props;
@@ -13,11 +27,12 @@ class Item extends Component {
 
     handleUpdate = () => {
         const { info, onUpdate } = this.props;
-        
+        this.setState({
+            updateBox: !this.state.updateBox
+        })
         onUpdate(info.id)
 
         console.log(this.state.updateBox)
-        
     }
 
     render() {
@@ -26,10 +41,18 @@ class Item extends Component {
         return (
             <div>
                 {this.state.updateBox ? 
-                    <div>
-                        <input />
-                        <input />
-                    </div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            onChange={this.handleChange} 
+                            value={this.state.name} 
+                            name="name"
+                        />
+                        <input 
+                            onChange={this.handleChange} 
+                            value={this.state.password} 
+                            name="password"
+                        />
+                    </form>
                     :
                     <div>{info.name}{info.password}</div>
                 } 
